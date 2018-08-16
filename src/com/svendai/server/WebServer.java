0d9b1a3a194fffb80d972d703467a318a603d81a
svendai <474192435@qq.com>
2018-08-17 00:19:44 +0800

    请求并响应

--- src/com/svendai/server/WebServer.java
+++ src/com/svendai/server/WebServer.java
@@ -1,8 +1,10 @@
 package com.svendai.server;
 
+import javax.xml.ws.http.HTTPBinding;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  * @program: SimpleHttpServer
@@ -12,7 +14,12 @@ import java.net.Socket;
  **/
 
 public class WebServer {
+    //Socket Server
     private ServerSocket server;
+    //换行
+    private final static String CLRF = "\r\n";
+    //空格
+    private static final String BLANK = " ";
 
     /**
      * 开启web服务
@@ -55,6 +62,11 @@ public class WebServer {
 
     }
 
+    /**
+     * 接收POST请求
+     * 由于http协议中 请求头和请求体之间有一个空行
+     * 所以直接用readline()的方法取不到post的参数
+     */
     private void receivePost(){
         try {
             //监听客户端连接
@@ -64,10 +76,47 @@ public class WebServer {
             String reqData  = new String(data,0,length);
 
             System.out.println(reqData);
+            //向客户端返回响应
+            this.response(client);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 向客户端返回响应
+     * @param client
+     * @throws IOException
+     */
+    private void response(Socket client) throws IOException{
+        //返回内容体
+        StringBuilder respContext = new StringBuilder();
+        respContext.append("<html><head><title>HTTP响应示例</title>" +
+                "</head><body>Hello world 你好世界!</body></html>");
+
+        //返回http头
+        StringBuilder response = new StringBuilder();
+        //1)  HTTP协议版本、状态代码、描述
+        response.append("HTTP/1.1").append(BLANK).append("200").append(BLANK).append("OK").append(CLRF);
+        //2)  响应头(Response Head)
+        response.append("Server:Tomcate Server/0.0.1").append(CLRF);
+        response.append("Date:").append(new Date()).append(CLRF);
+        response.append("Content-type:text/html;charset=UTF-8").append(CLRF);
+        //重要:返回正文的长度：字节单位
+        response.append("Content-Length:").append(respContext.toString().getBytes().length).append(CLRF);
+        //http头与正文之间的换行
+        response.append(CLRF);
+        //正文
+        response.append(respContext).append(CLRF);
+
+        System.out.println(response);
+
+        //输出流返回响应
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        bw.write(response.toString());
+        bw.flush();
+        bw.close();
     }
 
 
